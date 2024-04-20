cec2eb8cdcded72e2d8f3755838f114f2b6b42a8
far-fetched <p_panek@wp.pl>
2024-04-20 10:58:35 +0200

    with config file

--- lib/alternate.ex
+++ lib/alternate.ex
@@ -1,46 +1,42 @@
 defmodule Alternate do
   use GenServer
 
-
   def start_link(_) do
     GenServer.start_link(__MODULE__, false)
   end
 
   def schedule_next do
-    IO.puts 'schedule start'
+    IO.puts(~c"schedule start")
+
     Process.send_after(self(), :toggle, 5000)
-    IO.puts 'schedule end'
+    IO.puts(~c"schedule end")
   end
 
   @impl true
   def init(value) do
-    IO.puts 'init start'
-    {:ok, enable} = Circuits.GPIO.open("GPIO12", :output)
-    {:ok, dir} = Circuits.GPIO.open("GPIO13", :output)
-    {:ok, step} = Circuits.GPIO.open("GPIO19", :output)
-
+    IO.puts(~c"init start")
+    data = Alternate.GpioService.read_config() |> Alternate.GpioService.open_gpios()
     schedule_next()
-    IO.puts 'init end'
-    {:ok, { value, enable, dir, step }}
+    IO.puts(~c"init end")
+    {:ok, {data}}
   end
 
   @impl true
-  def handle_info(:toggle, { value, enable, dir, step }) do
-    #IO.puts 'state taki:'
-    IO.puts value
+  def handle_info(:toggle, {data}) do
+    IO.puts('data')
+    Alternate.GpioService.execute_action("on", 12, data)
+    Alternate.GpioService.execute_action("rising_edge", 19, data)
+    #Circuits.GPIO.write(enable, 1)
+    #Circuits.GPIO.write(dir, (value && 1) || 0)
 
-    Circuits.GPIO.write(enable, 1)
-    Circuits.GPIO.write(dir, value && 1 || 0)
+    #Enum.each(0..50, fn _x ->
+      #Circuits.GPIO.write(step, 0)
+      #:timer.sleep(2)
+      #Circuits.GPIO.write(step, 1)
+      #:timer.sleep(2)
+    #end)
 
-    Enum.each(0..50, fn(_x) ->
-      Circuits.GPIO.write(step, 0)
-      :timer.sleep(2);
-      Circuits.GPIO.write(step, 1)
-      :timer.sleep(2);
-    end)
-    #IO.puts 'end'
     schedule_next()
-    #IO.puts 'after reschedule'
-    {:noreply, { !value, enable, dir, step }}
+    {:noreply, {data}}
   end
 end
